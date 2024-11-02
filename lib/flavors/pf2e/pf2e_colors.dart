c47633ffc3368b9e96c901fa6e5028a7f0c9a751
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-01 21:07:42 -0300

    chore: add pf2e theme

---
+++ lib/flavors/pf2e/pf2e_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const mainRed = Color.fromRGBO(93, 0, 0, 1);
+const mainBlue = Color.fromRGBO(0, 37, 100, 1);
+const parchment = Color.fromRGBO(203, 193, 143, 1);
+const traitBorder = Color.fromRGBO(217, 196, 132, 1);
+const mainGreen = Color.fromRGBO(51, 79, 64, 1);
+const alingmentBlue = Color.fromRGBO(76, 80, 94, 1);
+const uncommonColor = Color.fromRGBO(102, 78, 71, 1);
+const uniqueColor = Color.fromRGBO(68, 53, 74, 1);
+const darkBackground = Color.fromRGBO(16, 15, 30, 1);
