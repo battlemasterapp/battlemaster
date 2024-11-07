6c16556bc5adb661844c669e22851a6c3a79f5eb
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-07 15:03:23 -0300

    chore: improve SelectedCombatants widget

--- lib/features/combatant/widgets/selected_combatants.dart
+++ lib/features/combatant/widgets/selected_combatants.dart
@@ -7,36 +7,75 @@ class SelectedCombatants extends StatelessWidget {
   const SelectedCombatants({
     super.key,
     this.combatants = const {},
+    this.onCombatantsChanged,
   });
 
   final Map<Combatant, int> combatants;
+  final ValueChanged<Map<Combatant, int>>? onCombatantsChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          AppLocalizations.of(context)!.combatants_selected,
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: combatants.length,
-            itemBuilder: (context, index) {
-              final combatant = combatants.keys.elementAt(index);
-              final count = combatants.values.elementAt(index);
-
-              return ListTile(
-                title: Text(combatant.name),
-                subtitle: Text(
-                  "$count",
-                ),
-              );
-            },
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 8,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            AppLocalizations.of(context)!.combatants_selected,
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
-        ),
-      ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: combatants.length,
+              itemBuilder: (context, index) {
+                final combatant = combatants.keys.elementAt(index);
+                final count = combatants.values.elementAt(index);
+
+                return Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      combatant.name,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    Spacer(),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            onPressed: () =>
+                                _updateCombatants(combatant, count - 1),
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text(count.toString()),
+                          IconButton(
+                            onPressed: () =>
+                                _updateCombatants(combatant, count + 1),
+                            icon: Icon(Icons.add),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _updateCombatants(Combatant combatant, int count) {
+    if (count == 0) {
+      combatants.remove(combatant);
+    } else {
+      combatants[combatant] = count;
+    }
+    onCombatantsChanged?.call(combatants);
   }
 }
