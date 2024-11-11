08b3e12967c342b4c60f7bc49a3e4729137c02b9
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-11 16:47:00 -0300

    chore: add skip dead behavior to EncounterTrackerNotifier

--- lib/features/settings/models/skip_dead_behavior.dart
+++ lib/features/settings/models/skip_dead_behavior.dart
@@ -1,5 +1,21 @@
+import 'package:battlemaster/features/combatant/models/combatant.dart';
+import 'package:battlemaster/features/combatant/models/combatant_type.dart';
+
 enum SkipDeadBehavior {
   all,
   allButPlayers,
   none,
+}
+
+extension SkipCombatants on SkipDeadBehavior {
+  bool shouldSkip(Combatant combatant) {
+    switch (this) {
+      case SkipDeadBehavior.all:
+        return !combatant.isAlive;
+      case SkipDeadBehavior.allButPlayers:
+        return combatant.type != CombatantType.player && !combatant.isAlive;
+      case SkipDeadBehavior.none:
+        return false;
+    }
+  }
 }
