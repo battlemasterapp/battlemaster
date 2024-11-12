2139ed4aa6ec0d4038cd27a8ef3afe38ca6a361f
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-12 18:51:37 -0300

    chore: show conditions on details panel

--- lib/features/conditions/widgets/add_condition_button.dart
+++ lib/features/conditions/widgets/add_condition_button.dart
@@ -1,7 +1,8 @@
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:battlemaster/features/conditions/models/condition.dart';
 import 'package:battlemaster/features/conditions/widgets/add_condition_dialog.dart';
+import 'package:battlemaster/features/conditions/widgets/conditions_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 class AddConditionButton extends StatelessWidget {
@@ -17,22 +18,35 @@ class AddConditionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final localization = AppLocalizations.of(context)!;
-    return OutlinedButton.icon(
-      style: OutlinedButton.styleFrom(
-        foregroundColor: Theme.of(context).colorScheme.secondary,
-      ),
-      onPressed: () async {
-        final selectedConditions = await showDialog<List<Condition>?>(
-          context: context,
-          builder: (context) => AddConditionDialog(conditions: conditions),
-        );
+    return Wrap(
+      spacing: 8,
+      runSpacing: 8,
+      children: [
+        for (final condition in conditions)
+          ConditionChip(
+            condition: condition,
+            onDeleted: () {
+              onConditionsAdded?.call(conditions..remove(condition));
+            },
+          ),
+        OutlinedButton.icon(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Theme.of(context).colorScheme.secondary,
+          ),
+          onPressed: () async {
+            final selectedConditions = await showDialog<List<Condition>?>(
+              context: context,
+              builder: (context) => AddConditionDialog(conditions: conditions),
+            );
 
-        if (selectedConditions != null) {
-          onConditionsAdded?.call(selectedConditions);
-        }
-      },
-      icon: Icon(MingCute.add_fill),
-      label: Text(localization.add_condition_button),
+            if (selectedConditions != null) {
+              onConditionsAdded?.call(selectedConditions);
+            }
+          },
+          icon: Icon(MingCute.add_fill),
+          label: Text(localization.add_condition_button),
+        ),
+      ],
     );
   }
 }
--- lib/features/conditions/widgets/conditions_list.dart
+++ lib/features/conditions/widgets/conditions_list.dart
@@ -1,5 +1,35 @@
 import 'package:battlemaster/features/conditions/models/condition.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ConditionChip extends StatelessWidget {
+  const ConditionChip({
+    super.key,
+    required this.condition,
+    this.onDeleted,
+  });
+
+  final Condition condition;
+  final VoidCallback? onDeleted;
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalizations.of(context)!;
+    String label = condition.name;
+    if (condition.value != null) {
+      label += ' ${condition.value}';
+    }
+    if (condition.durationRounds != null) {
+      label +=
+          ' ${localization.condition_duration_round(condition.durationRounds!)}';
+    }
+
+    return Chip(
+      label: Text(label),
+      onDeleted: onDeleted,
+    );
+  }
+}
 
 class ConditionsList extends StatelessWidget {
   const ConditionsList({super.key, this.conditions = const []});
@@ -12,11 +42,7 @@ class ConditionsList extends StatelessWidget {
       spacing: 4,
       runSpacing: 4,
       children: [
-        for (final condition in conditions)
-        // TODO: format name
-          Chip(
-            label: Text(condition.name),
-          ),
+        for (final condition in conditions) ConditionChip(condition: condition),
       ],
     );
   }
