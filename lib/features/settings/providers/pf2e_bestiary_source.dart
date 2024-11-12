171331974a87575be7d90b5744a753a3a40aafa1
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-12 11:52:37 -0300

    style: format

--- lib/features/settings/providers/pf2e_bestiary_source.dart
+++ lib/features/settings/providers/pf2e_bestiary_source.dart
@@ -24,9 +24,11 @@ class Pf2eBestiarySource extends ChangeNotifier {
   Future<void> load() async {
     _status = Pf2eBestiarySourceStatus.loading;
     notifyListeners();
-    final dio = Dio(BaseOptions(baseUrl: const String.fromEnvironment('PF2E_URI')));
+    final dio =
+        Dio(BaseOptions(baseUrl: const String.fromEnvironment('PF2E_URI')));
     final response = await dio.get('/bestiaries/index.json');
-    final sources = (jsonDecode(response.data) as Map).keys.cast<String>().toSet();
+    final sources =
+        (jsonDecode(response.data) as Map).keys.cast<String>().toSet();
     _bestiaries = sources;
     _status = Pf2eBestiarySourceStatus.loaded;
     notifyListeners();
