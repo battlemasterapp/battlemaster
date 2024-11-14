1a84fe6ec3d540b351e689352cefa9c9201237fb
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-14 09:24:21 -0300

    chore: fix quick health mod buttons in portrait mode

---
+++ lib/features/shared/colors.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+Color getHealthColor(int currentHp, {int maxHp = 0}) {
+  const fullHealthColor = Colors.green;
+  const halfHealthColor = Colors.yellow;
+  const deadColor = Colors.red;
+
+  if (currentHp == 0) {
+    return deadColor;
+  }
+
+  if (maxHp == 0) {
+    return Color.lerp(
+    fullHealthColor,
+    deadColor,
+    1 - (currentHp / (max(maxHp, currentHp))),
+  )!;
+  }
+
+  final halfHealth = maxHp / 2;
+
+  if (currentHp >= halfHealth) {
+    return Color.lerp(
+      fullHealthColor,
+      halfHealthColor,
+      1 - ((currentHp - halfHealth) / halfHealth),
+    )!;
+  }
+
+  return Color.lerp(
+    halfHealthColor,
+    deadColor,
+    1 - (currentHp / (max(maxHp, currentHp) - halfHealth)),
+  )!;
+}
