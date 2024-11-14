8aa95cccca785d8404683ee7ae22f13842995e07
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-14 19:06:38 -0300

    chore: update custom condition page ui

--- lib/features/conditions/custom_conditions_page.dart
+++ lib/features/conditions/custom_conditions_page.dart
@@ -2,9 +2,9 @@ import 'package:battlemaster/database/database.dart';
 import 'package:battlemaster/features/conditions/providers/conditions_provider.dart';
 import 'package:battlemaster/features/conditions/widgets/create_condition_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CustomConditionsPage extends StatelessWidget {
   const CustomConditionsPage({super.key});
@@ -49,25 +49,52 @@ class CustomConditionsPage extends StatelessWidget {
                 );
               }
 
-              return ListView.separated(
-                itemCount: conditions.length,
-                padding: const EdgeInsets.only(bottom: 60),
-                separatorBuilder: (_, __) => const Divider(),
-                itemBuilder: (context, index) {
-                  final condition = conditions[index];
-                  return ListTile(
-                    title: Text(condition.name),
-                    subtitle: Text(condition.description),
-                    trailing: IconButton(
-                      icon: Icon(MingCute.delete_2_fill),
-                      onPressed: () async {
-                        await context
-                            .read<ConditionsProvider>()
-                            .deleteCondition(condition);
-                      },
-                    ),
-                  );
-                },
+              return Center(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 600),
+                  child: ListView.separated(
+                    itemCount: conditions.length,
+                    padding: const EdgeInsets.only(bottom: 80, top: 8),
+                    separatorBuilder: (_, __) => const SizedBox(height: 12),
+                    itemBuilder: (context, index) {
+                      final condition = conditions[index];
+                      return Card(
+                        margin: const EdgeInsets.all(8),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(condition.name,
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium),
+                              const Divider(),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Expanded(child: Text(condition.description)),
+                                  Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: IconButton(
+                                      icon: Icon(MingCute.delete_2_fill),
+                                      onPressed: () async {
+                                        await context
+                                            .read<ConditionsProvider>()
+                                            .deleteCondition(condition);
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               );
             }),
       ),
