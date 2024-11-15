8eeb618b502ad2f152aef376f47295084554ca7c
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-15 11:57:04 -0300

    chore: remove sentry dio integration
    Caused a cors bug

--- lib/api/services/data_service.dart
+++ lib/api/services/data_service.dart
@@ -6,7 +6,6 @@ import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sentry_dio/sentry_dio.dart';
 
 
 abstract class DataService<T> {
@@ -32,7 +31,6 @@ abstract class DataService<T> {
     client = Dio(BaseOptions(baseUrl: baseUrl))
       ..interceptors.add(
           DioCacheInterceptor(options: CacheOptions(store: MemCacheStore())));
-    client.addSentry();
   }
 
   Future<T?> fetchData({bool forceRefresh = false});
