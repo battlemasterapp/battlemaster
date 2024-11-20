aa4ab2981517d8e71a76d7d906381f4f8a9c8e1e
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-20 20:48:12 -0300

    chore: add empty state for add group combatants

--- lib/features/combatant/widgets/add_group_combatants.dart
+++ lib/features/combatant/widgets/add_group_combatants.dart
@@ -1,8 +1,11 @@
+import 'package:battlemaster/features/analytics/analytics_service.dart';
 import 'package:battlemaster/features/encounters/providers/encounters_provider.dart';
+import 'package:battlemaster/features/engines/models/game_engine_type.dart';
+import 'package:battlemaster/features/groups/group_detail_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../encounters/models/encounter.dart';
 import '../../encounters/models/encounter_type.dart';
@@ -26,11 +29,13 @@ class _AddGroupCombatantsState extends State<AddGroupCombatants> {
   @override
   Widget build(BuildContext context) {
     final state = context.read<EncountersProvider>();
+    final localization = AppLocalizations.of(context)!;
+    final analytics = context.read<AnalyticsService>();
     return Column(
       children: [
         TextField(
           decoration: InputDecoration(
-            labelText: AppLocalizations.of(context)!.search_input,
+            labelText: localization.search_input,
             prefixIcon: Icon(MingCute.search_fill),
           ),
           onChanged: (value) => setState(() => _search = value),
@@ -45,7 +50,7 @@ class _AddGroupCombatantsState extends State<AddGroupCombatants> {
                     .where((group) => group.name.contains(_search))
                     .toList();
                 if (filteredGroups.isEmpty) {
-                  return Text(AppLocalizations.of(context)!.empty_groups_list);
+                  return const _EmptyState();
                 }
                 return ListView.builder(
                   itemCount: filteredGroups.length,
@@ -66,6 +71,45 @@ class _AddGroupCombatantsState extends State<AddGroupCombatants> {
                 );
               }),
         ),
+      ],
+    );
+  }
+}
+
+class _EmptyState extends StatelessWidget {
+  const _EmptyState();
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalizations.of(context)!;
+    return Column(
+      children: [
+        Text(localization.empty_groups_list),
+        const SizedBox(height: 8),
+        OutlinedButton(
+          onPressed: () async {
+            final encounter = Encounter(
+              name: AppLocalizations.of(context)!.new_group_name,
+              type: EncounterType.group,
+              combatants: [],
+              engine: GameEngineType.pf2e,
+            );
+            final created = await context
+                .read<EncountersProvider>()
+                .addEncounter(encounter);
+            // ignore: use_build_context_synchronously
+            Navigator.of(context).pushNamed(
+              "/group",
+              arguments: GroupDetailPageParams(encounter: created),
+            );
+            // ignore: use_build_context_synchronously
+            await context.read<AnalyticsService>().logEvent(
+              'create-encounter',
+              props: {'type': EncounterType.group.toString()},
+            );
+          },
+          child: Text(localization.create_group_cta),
+        )
       ],
     );
   }
