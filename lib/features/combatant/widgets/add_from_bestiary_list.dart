4bf0e82004c4c424057ffcbf79876ea1ad354bb5
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-20 21:06:23 -0300

    feat: add empty state for AddFromBestiaryList

--- lib/features/combatant/widgets/add_from_bestiary_list.dart
+++ lib/features/combatant/widgets/add_from_bestiary_list.dart
@@ -20,33 +20,54 @@ class AddFromBestiaryList extends StatefulWidget {
 }
 
 class _AddFromBestiaryListState extends State<AddFromBestiaryList> {
-  String _searchTerm = "";
+  final _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final localization = AppLocalizations.of(context)!;
 
-    final filteredCombatants = widget.combatants.where((combatant) {
-      return combatant.name.toLowerCase().contains(_searchTerm.toLowerCase());
-    }).toList();
-
     return Column(
       children: [
         TextField(
+          controller: _searchController,
           decoration: InputDecoration(
             labelText: localization.search_input,
             hintText: localization.search_input,
             prefixIcon: Icon(MingCute.search_fill),
+            suffix: IconButton(
+              onPressed: () => setState(() => _searchController.clear()),
+              icon: Icon(MingCute.close_fill),
+            ),
           ),
-          onChanged: (value) => setState(() => _searchTerm = value),
+          onChanged: (_) => setState(() {}),
         ),
         const SizedBox(height: 8),
-        Expanded(
-          child: BestiaryList(
-            combatants: filteredCombatants,
-            onCombatantSelected: widget.onCombatantSelected,
-          ),
-        ),
+        Builder(builder: (context) {
+          final filteredCombatants = widget.combatants.where((combatant) {
+            return combatant.name
+                .toLowerCase()
+                .contains(_searchController.text.toLowerCase());
+          }).toList();
+
+          if (filteredCombatants.isEmpty) {
+            return Center(
+              child: Text(localization.no_results_search),
+            );
+          }
+
+          return Expanded(
+            child: BestiaryList(
+              combatants: filteredCombatants,
+              onCombatantSelected: widget.onCombatantSelected,
+            ),
+          );
+        }),
       ],
     );
   }
