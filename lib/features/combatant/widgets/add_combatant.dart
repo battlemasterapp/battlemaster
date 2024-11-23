9c4f33d7fa181140bff124cabdde65319e75815c
Vytor Calixto <vytorcalixto@gmail.com>
2024-11-23 16:24:59 -0300

    feat: display custom bestiaries to pick combatants from

--- lib/features/combatant/widgets/add_combatant.dart
+++ lib/features/combatant/widgets/add_combatant.dart
@@ -1,19 +1,22 @@
 import 'package:battlemaster/api/providers/dnd5e_engine_provider.dart';
 import 'package:battlemaster/api/services/pf2e_bestiary_service.dart';
 import 'package:battlemaster/features/analytics/analytics_service.dart';
+import 'package:battlemaster/features/bestiaries/models/custom_bestiary.dart';
+import 'package:battlemaster/features/bestiaries/providers/custom_bestiary_provider.dart';
+import 'package:battlemaster/features/combatant/models/combatant.dart';
 import 'package:battlemaster/features/combatant/widgets/add_custom_combatant.dart';
 import 'package:battlemaster/features/combatant/widgets/add_from_bestiary_list.dart';
 import 'package:battlemaster/features/combatant/widgets/add_group_combatants.dart';
 import 'package:battlemaster/features/settings/providers/system_settings_provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:battlemaster/features/combatant/models/combatant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:provider/provider.dart';
 
 enum AddCombatantSource {
   dnd5e,
   pf2e,
+  customBestiary,
   group,
   custom,
 }
@@ -47,7 +50,7 @@ class _AddCombatantState extends State<AddCombatant> {
       _selected.add(AddCombatantSource.dnd5e);
     }
     if (_selected.isEmpty) {
-      _selected.add(AddCombatantSource.group);
+      _selected.add(AddCombatantSource.customBestiary);
     }
   }
 
@@ -74,6 +77,11 @@ class _AddCombatantState extends State<AddCombatant> {
                   label: Text(localization.pf2e_toggle_button),
                   icon: Icon(FontAwesome.dragon_solid),
                 ),
+              ButtonSegment(
+                value: AddCombatantSource.customBestiary,
+                label: Text(localization.custom_bestiary_toggle_button),
+                icon: Icon(MingCute.paw_fill),
+              ),
               ButtonSegment(
                 value: AddCombatantSource.group,
                 label: Text(localization.groups_toggle_button),
@@ -119,6 +127,28 @@ class _AddCombatantState extends State<AddCombatant> {
         },
       );
     }
+
+    if (_selected.contains(AddCombatantSource.customBestiary)) {
+      return StreamBuilder<List<CustomBestiary>>(
+          stream: context.read<CustomBestiaryProvider>().watchAll(),
+          builder: (context, snapshot) {
+            final data = snapshot.data ?? [];
+            return AddFromBestiaryList(
+              combatants: data.fold(
+                [],
+                (combatants, bestiary) =>
+                    combatants..addAll(bestiary.combatants),
+              )..sort((a, b) => a.name.compareTo(b.name)),
+              onCombatantSelected: (combatant) async {
+                widget.onCombatantsAdded({combatant: 1});
+                await context
+                    .read<AnalyticsService>()
+                    .logEvent('add_custom_bestiary_combatant');
+              },
+            );
+          });
+    }
+
     if (_selected.contains(AddCombatantSource.group)) {
       return AddGroupCombatants(
         onGroupSelected: (combatants) async {
