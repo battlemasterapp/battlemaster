5540a2230973f236f3bfec68877f84f4cb528d4c
Vytor Calixto <vytorcalixto@gmail.com>
2024-12-09 18:12:39 -0300

    fix: only enable analytics on release mode

--- lib/features/analytics/plausible.dart
+++ lib/features/analytics/plausible.dart
@@ -1,4 +1,5 @@
 import 'package:battlemaster/features/analytics/plausible_base.dart';
+import 'package:flutter/foundation.dart';
 
 const _server = String.fromEnvironment("PLAUSIBLE_SERVER");
 const _domain = String.fromEnvironment("PLAUSIBLE_DOMAIN");
@@ -6,4 +7,5 @@ const _domain = String.fromEnvironment("PLAUSIBLE_DOMAIN");
 final plausible = Plausible(
   server: Uri.parse(_server),
   domain: _domain,
+  isActive: kReleaseMode,
 );
