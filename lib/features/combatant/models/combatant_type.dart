7e9f0d5883b9625f7054497733d74b15c92224ec
Vytor Calixto <vytorcalixto@gmail.com>
2024-12-09 18:12:38 -0300

    feat: combatant type indicator (#36)
    * chore: add icon lib
    
    * chore: create extension to get CombatantType icon
    
    * chore: add combatant type icon to TrackerTile
    
    * chore: add CombatantType icons to add custom combatant dropdown
    
    * style: format

--- lib/features/combatant/models/combatant_type.dart
+++ lib/features/combatant/models/combatant_type.dart
@@ -1,6 +1,23 @@
+import 'package:ultimate_flutter_icons/flutter_icons.dart';
+
 enum CombatantType {
   player,
   monster,
   hazard,
   lair,
+}
+
+extension CombatantTypeIcon on CombatantType {
+  FIconObject get icon {
+    switch (this) {
+      case CombatantType.player:
+        return GI.GiVisoredHelm;
+      case CombatantType.monster:
+        return GI.GiSpikedDragonHead;
+      case CombatantType.hazard:
+        return GI.GiWolfTrap;
+      case CombatantType.lair:
+        return GI.GiStalactites;
+    }
+  }
 }
