871dceb77e68369a7ec0f6e1766c4870e6cb567d
Vytor Calixto <vytorcalixto@gmail.com>
2024-12-09 18:12:37 -0300

    feat: turn dark mode an app setting

--- lib/features/settings/providers/system_settings_provider.dart
+++ lib/features/settings/providers/system_settings_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -5,20 +6,24 @@ import '../models/initiative_roll_type.dart';
 
 class SystemSettings extends ChangeNotifier {
   InitiativeRollType _rollType = InitiativeRollType.manual;
+  ThemeMode _themeMode = ThemeMode.system;
 
   SystemSettings() {
     _init();
   }
 
-  InitiativeRollType get rollType => _rollType;
-
   Future<void> _init() async {
     // Load settings from disk
     final preferences = await SharedPreferences.getInstance();
     _rollType = InitiativeRollType.values[preferences.getInt('rollType') ?? 0];
+    _themeMode = ThemeMode.values[preferences.getInt('themeMode') ?? 0];
 
     notifyListeners();
   }
+
+  InitiativeRollType get rollType => _rollType;
+
+  ThemeMode get themeMode => _themeMode;
 
   Future<void> setInitiativeRollType(InitiativeRollType rollType) async {
     _rollType = rollType;
@@ -27,5 +32,14 @@ class SystemSettings extends ChangeNotifier {
     // Save settings to disk
     final preferences = await SharedPreferences.getInstance();
     await preferences.setInt('rollType', rollType.index);
+  }
+
+  Future<void> setThemeMode(ThemeMode themeMode) async {
+    _themeMode = themeMode;
+    notifyListeners();
+
+    // Save settings to disk
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setInt('themeMode', _themeMode.index);
   }
 }
