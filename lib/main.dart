97923dc1e3abd69fbd5ddd9e2d1b25b2c540652a
Vytor Calixto <vytorcalixto@gmail.com>
2024-12-09 18:12:36 -0300

    style: format code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:battlemaster/features/main_page/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'features/combats/combats_page.dart';
-import 'features/groups/groups_page.dart';
-import 'features/settings/settings_page.dart';
+import 'features/main_page/main_page.dart';
 import 'flavors/pf2e/pf2e_theme.dart';
 
 void main() {
@@ -25,8 +22,6 @@ class BattlemasterApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       routes: {
         "/": (context) => const MainPage(),
-        "/groups": (context) => const GroupsPage(),
-        "/settings": (context) => const SettingsPage(),
       },
       initialRoute: "/",
     );
